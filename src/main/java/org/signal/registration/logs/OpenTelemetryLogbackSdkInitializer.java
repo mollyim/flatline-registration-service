d4de7150e3ff118a7230731a4962c2e143b9262d
Chris Eager <chris@signal.org>
2026-01-07 17:19:22 -0600

    Add OpenTelemetry logging

---
+++ src/main/java/org/signal/registration/logs/OpenTelemetryLogbackSdkInitializer.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2026 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.signal.registration.logs;
+
+import io.micronaut.context.annotation.Context;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import javax.annotation.PostConstruct;
+
+@Context
+class OpenTelemetryLogbackSdkInitializer {
+
+  private final OpenTelemetry openTelemetry;
+
+  OpenTelemetryLogbackSdkInitializer(final OpenTelemetry openTelemetry) {
+    this.openTelemetry = openTelemetry;
+  }
+
+  @PostConstruct
+  void install() {
+    OpenTelemetryAppender.install(openTelemetry);
+  }
+
+}
